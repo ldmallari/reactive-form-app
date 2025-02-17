b587f2967f0cfba26a5ebf1a57883b2a537f4f31
Yuji <mallarilevin@gmail.com>
2025-02-17 13:21:05 +0800

    added extra fields and custom css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'reactive-form-app';
+  userName: string = "";
+  email: string = "";
+  password: string = "";
+  age: number | null = null;
+
+  formdata: FormGroup = new FormGroup({
+    userName: new FormControl(""),
+    email: new FormControl(""),
+    password: new FormControl(""),
+    age: new FormControl("")
+  });
+
+  onClickSubmit(data: { userName: string; email: string; password: string; age: number }) {
+    this.userName = data.userName;
+    this.email = data.email;
+    this.password = data.password;
+    this.age = data.age;
+  }
 }
